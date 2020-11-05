a47861bc899541be29aa38a373a34fbe8af682eb
Madeva Tuppad <madtupp1@in.ibm.com>
2020-11-05 18:58:02 +0530

    Assignment-7 ( myapp with catagories listed on products and added the selected categores toproducts)

---
+++ my-app/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import Products from "./products";
+import Categories from "./categories";
+
+import store from "./store";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Categories />
+      <Products />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
---
+++ my-app/src/products/views/ProductEditor.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class ProductEditor extends Component {
+    state = {
+        name: '',
+        description: '',
+        price: 0,
+        category:''
+    };
+    componentDidMount() {
+        const   {categories}=this.props;
+        this.setState({
+            category: categories && categories.lngth>0?categories[0].name:''
+        })
+    }
+    onAddNewProductClick = () => {
+        const { addNew,categories } = this.props,
+            { name, description, price ,category} = this.state;
+            let selectedCatgoy=category;
+            if(categories && categories.length>0){
+                let catg= categories.find(cat=>cat.id===selectedCatgoy)
+                selectedCatgoy=catg?catg.name:categories[0].name
+            }
+        addNew(name, description, price,selectedCatgoy);
+    };
+    render() {
+        const   {categories}=this.props;
+       
+        return (
+            <section className="edit">
+                <div className="field">
+                    <label htmlFor="">Name :</label>
+                    <input type="text" onChange={evt => this.setState({ name: evt.target.value })} />
+                </div>
+                <div className="field">
+                    <label htmlFor="">Description :</label>
+                    <input type="text" onChange={evt => this.setState({ description: evt.target.value })} />
+                </div>
+                <div className="field">
+                    <label htmlFor="">Price :</label>
+                    <input type="number" onChange={evt => this.setState({ price: parseInt(evt.target.value) })} />
+                </div>
+                <div className="field">
+                    <label htmlFor="">Category</label>
+                    <select onChange={evt => this.setState({ category: evt.target.value})}>
+                        {categories.map(cat => <option  key={cat.id} value={cat.id}>{cat.name}</option>)}
+                    </select>
+                </div>
+                <div className="field">
+                    <input type="button" value="Add Product" onClick={this.onAddNewProductClick} />
+                </div>
+            </section>
+        )
+    }
+}
+
+export default ProductEditor;
